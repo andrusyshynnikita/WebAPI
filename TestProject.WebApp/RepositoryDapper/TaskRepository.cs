2ceaed65c59ffd33fca9de2ea65fe72ec6db8bf8
Anuitex <Anuitex@DESKTOP-8E5OABS>
2019-02-21 14:35:52 +0200

    with dapper

--- TestProject.WebApp/RepositoryDapper/TaskRepository.cs
+++ TestProject.WebApp/RepositoryDapper/TaskRepository.cs
@@ -10,13 +10,19 @@ using System.Web;
 using TestProject.WebApp.Interface;
 using TestProject.WebApp.Models;
 using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace TestProject.WebApp.RepositoryDapper
 {
     public class TaskRepository : ITaskRepository<TaskModel>
     {
         string connectionString =  ConfigurationManager.ConnectionStrings["BookContext"].ConnectionString;
-
+        private TaskModel _taskModel;
+        private string _filename;
 
         public TaskRepository()
         {
@@ -27,7 +33,8 @@ namespace TestProject.WebApp.RepositoryDapper
         {
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                return db.Query<TaskModel>("SELECT * FROM Users WHERE User_Id = @id", new { id }).ToList();
+              var res=  SqlMapperExtensions.GetAll<TaskModel>(db).Where(x => x.User_Id == id);
+                return res;
             }
         }
 
@@ -35,14 +42,36 @@ namespace TestProject.WebApp.RepositoryDapper
         {
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age)";
-                //  db.Execute(sqlQuery, user);
+                try
+                {
+                    if (httpRequest.Files.Count > 0)
+                    {
+                        foreach (string file in httpRequest.Files)
+                        {
+                            var postedFile = httpRequest.Files[file];
 
-                // если мы хотим получить id добавленного пользователя
-                //var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
-                //int? userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
-                //user.Id = userId.Value;
-                return "hello";
+                            _filename = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+
+                            var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + _filename);
+
+                            postedFile.SaveAs(filePath);
+
+                        }
+                    }
+
+                    if (httpRequest["TaskModel"] != null)
+                    {
+                        var postedForm = httpRequest.Form["TaskModel"];
+                        _taskModel = JsonConvert.DeserializeObject<TaskModel>(postedForm);
+                        SqlMapperExtensions.Insert(db, _taskModel);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    return exception.Message;
+                }
+
+                return "no files";
             }
         }
 
@@ -50,24 +79,80 @@ namespace TestProject.WebApp.RepositoryDapper
         {
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
-             //   db.Execute(sqlQuery, user);
+                try
+                {
+                    if (httpRequest.Files.Count > 0)
+                    {
+                        foreach (string file in httpRequest.Files)
+                        {
+                            var postedFile = httpRequest.Files[file];
+
+                            var fileName = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+
+                            var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + fileName);
+
+                            postedFile.SaveAs(filePath);
+                        }
+                    }
+
+                    if (httpRequest["TaskModel"] != null)
+                    {
+                        var postedForm = httpRequest.Form["TaskModel"];
+                        _taskModel = JsonConvert.DeserializeObject<TaskModel>(postedForm);
+                        SqlMapperExtensions.Update(db, _taskModel);
+                    }
+                }
+
+                catch (Exception exception)
+                {
+                    return exception.Message;
+                }
+
+                return "no files";
             }
-            return "hello";
         }
 
         public void Delete(int id)
         {
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                var sqlQuery = "DELETE FROM Users WHERE Id = @id";
-                db.Execute(sqlQuery, new { id });
+                TaskModel task = SqlMapperExtensions.Get<TaskModel>(db, id);
+
+                if (task.AudioFileName != null)
+                {
+                    var fileName = task.AudioFileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+
+                    var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + fileName);
+
+                    File.Delete(filePath);
+                }
+
+                if (task != null)
+                {
+                    SqlMapperExtensions.Delete<TaskModel>(db, task);
+                    
+                }
             }
         }
 
         public HttpResponseMessage DownloadAudioFile(int id)
         {
-            throw new NotImplementedException();
+            IDbConnection db = new SqlConnection(connectionString);
+
+            var task = SqlMapperExtensions.Get<TaskModel>(db, id); 
+
+            var fileName = task.AudioFileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+
+            var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + fileName);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            response.Content = new StreamContent(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = fileName;
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/3gpp");
+
+            return response;
         }
     }
 }
